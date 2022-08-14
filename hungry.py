f0ab4af13feca9ef621d278638bc299ee14a7d2d
avinashnt <avinashnt@gmail.com>
2022-08-14 12:27:01 -0400

    Update hungry.py

--- hungry.py
+++ hungry.py
@@ -1,5 +1,5 @@
 hungry=input("are you hungry?")
-if hungry=="yes" and hungry=="y" and hungry=="Y":
+if hungry=="yes" and hungry=="y" and hungry=="YY":
     print("eat samosa")
 else:
     print("do homework")    
